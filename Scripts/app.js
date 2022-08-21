3243ca964560116aea5aff789e7098857c2952df
nayem9b <razwanniam1@gmail.com>
2022-08-22 00:26:05 +0600

    calculation section added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,18 @@
-console.log('Js file connected')
+document.getElementById('calculate-button').addEventListener('click', function(){
+  const perPlayerCostString= document.getElementById('perPlayerCost').value;
+  const perPlayerCostValue= parseInt(perPlayerCostString);
+  const player= document.getElementById('expenses-span-value').innerText= perPlayerCostValue;
+})
+
+document.getElementById('calculate-total').addEventListener('click',function(){
+    const managerCostString= document.getElementById('manager-cost').value;
+    const managerCostValue= parseInt(managerCostString);
+    
+    console.log(managerCostValue);
+    
+    const coachCostString= document.getElementById('coach-cost').value;
+    const coachCostValue= parseInt(coachCostString);
+    console.log(coachCostValue);
+
+    const total = document.getElementById('total').innerText= (managerCostValue+ coachCostValue);
+})
